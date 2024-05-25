d3f2026a18473f54d679ef3f1e14828f0ebadf39
Roman Pena <roman.pena@inchworks.net>
2024-05-25 16:04:08 -0400

    Added input parameters for directory and repository

--- download-release/download.sh
+++ download-release/download.sh
@@ -1,18 +1,26 @@
 # Name of the release
 RELEASE_NAME=$1
+# The directory to download files into
+DIRECTORY=$2
 # Glob pattern to match the assets to be downloaded from the release
-ASSETS_PATTERN=$2
-
+ASSETS_PATTERN=$3
+# GitHub repository
+REPOSITORY=$4
 
 if [ -z "${RELEASE_NAME}" ]; then
     echo "ERROR :: Release name is required."
     exit 1;
 fi
 
-echo "Downloading release $RELEASE_NAME."
+if [ -z "${REPOSITORY}" ]; then
+    echo "ERROR :: Repository is required."
+    exit 1;
+fi
+
+echo "Downloading release $RELEASE_NAME from $REPOSITORY to $DIRECTORY."
 
 if [ -z "$ASSETS_PATTERN" ]; then
-    gh release download $RELEASE_NAME
+    gh release download $RELEASE_NAME --dir $DIRECTORY --repo $REPOSITORY
 else
-    gh release download $RELEASE_NAME --pattern $ASSETS_PATTERN
+    gh release download $RELEASE_NAME --dir $DIRECTORY --pattern $ASSETS_PATTERN --repo $REPOSITORY
 fi
